75e0144ad434bea4493f84ec735be692fb4a7df5
Felix Gebauer <felix.gebauer@optiz-consulting.com>
2026-09-17 16:44:06 +0200

    adds new front page

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the app name', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, communityday');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Community Day Slot Manager');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,7 +6,8 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('communityday');
+  protected readonly appName = signal('Community Day Slot Manager');
 }
